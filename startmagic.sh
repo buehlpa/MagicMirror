f7a79973e3b723c666493f8ddec82d374b0074dd
Pascal Simon Bühler <64488738+buehlpa@users.noreply.github.com>
2024-05-11 12:39:53 +0200

    Add files via upload

---
+++ startmagic.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+echo "Starting script"
+lxterminal --command="bash -c 'cd /home/USER//MagicMirror/ && pwd && npm run start'"
---
+++ termina_control.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+source /home/USER/.bash_profile
+
+# Open a terminal emulator and run the Python script
+lxterminal --command="/usr/bin/python3 /home/USER/startupscripts/control.py"
+
